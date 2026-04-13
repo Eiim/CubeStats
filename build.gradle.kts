38c099660552372f0c920423a71de1b3f63f4fcb
Ethan Chapman <ethan@we3chapmans.com>
2026-04-13 19:05:32 -0400

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,18 @@ repositories {
 }
 
 dependencies {
-	implementation("com.google.code.gson:gson:2.13.1")
-	implementation("org.mariadb.jdbc:mariadb-java-client:3.5.5")
-	implementation("org.eclipse.jetty:jetty-server:12.1.1")
-	implementation("org.eclipse.jetty.compression:jetty-compression-server:12.1.1")
-	implementation("org.eclipse.jetty.http2:jetty-http2-server:12.1.1")
-	implementation("org.eclipse.jetty:jetty-alpn-server:12.1.1")
-	implementation("org.eclipse.jetty:jetty-alpn-java-server:12.1.1")
-	implementation("org.eclipse.jetty.http3:jetty-http3-server:12.1.1")
-	implementation("org.eclipse.jetty.quic:jetty-quic-quiche-server:12.1.1")
-	implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.25.1")
-	implementation("org.apache.logging.log4j:log4j-core:2.25.1")
-	implementation("org.apache.logging.log4j:log4j-api:2.25.1")
+	implementation("com.google.code.gson:gson:2.13.2")
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.5.8")
+	implementation("org.eclipse.jetty:jetty-server:12.1.8")
+	implementation("org.eclipse.jetty.compression:jetty-compression-server:12.1.8")
+	implementation("org.eclipse.jetty.http2:jetty-http2-server:12.1.8")
+	implementation("org.eclipse.jetty:jetty-alpn-server:12.1.8")
+	implementation("org.eclipse.jetty:jetty-alpn-java-server:12.1.8")
+	implementation("org.eclipse.jetty.http3:jetty-http3-server:12.1.8")
+	implementation("org.eclipse.jetty.quic:jetty-quic-quiche-server:12.1.8")
+	implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.25.4")
+	implementation("org.apache.logging.log4j:log4j-core:2.25.4")
+	implementation("org.apache.logging.log4j:log4j-api:2.25.4")
 }
 
 java {
